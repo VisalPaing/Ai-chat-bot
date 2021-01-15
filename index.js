dce871e0cc1c64ead32c0a7a69be4f1ddf84baed
Paing_Visal <73647971+VisalPaing@users.noreply.github.com>
2021-01-15 11:29:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const alexa = require("alexa-bot-api");
+var chatbot = new alexa("aw2plm");
+const config = require("./config.json")
+
+client.once("ready", () => {
+  console.log("ChatBot online!");
+  });
+client.on("message", async message => {
+  if (message.author.bot) return;
+    let content = message.content;
+      if(!content) return;
+          chatbot.getReply(content).then(r => message.channel.send(r));
+            });
+client.login(config.token);
